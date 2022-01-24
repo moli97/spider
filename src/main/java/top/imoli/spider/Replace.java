1880dd30edc137e802d5651b60a77f2f66851fda
moli <moli@hulai.com>
2022-01-24 23:56:40 +0800

    处理无换行符

---
+++ src/main/java/top/imoli/spider/Replace.java
@@ -0,0 +1,34 @@
+package top.imoli.spider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Replace {
+
+    public static final String DEFAULT_TARGET = "";
+    public final String source;
+    public final String target;
+
+    public Replace(String source) {
+        this(source, DEFAULT_TARGET);
+    }
+
+    public Replace(String source, String target) {
+        this.source = source;
+        this.target = target;
+    }
+
+    public static final List<Replace> list = new ArrayList<>();
+
+    static {
+        list.add(new Replace("一秒记住【笔趣看 www.biqukan.cc】，精彩小说无弹窗免费阅读！"));
+        list.add(new Replace("\\s{2,}", "\n"));
+    }
+
+    public static String handle(String text) {
+        for (Replace replace : list) {
+            text = text.replaceAll(replace.source, replace.target);
+        }
+        return text;
+    }
+}
--- src/main/java/top/imoli/spider/task/ChapterTask.java
+++ src/main/java/top/imoli/spider/task/ChapterTask.java
@@ -2,10 +2,12 @@ package top.imoli.spider.task;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import top.imoli.spider.Chapter;
-import top.imoli.spider.Discard;
+import top.imoli.spider.Replace;
 
-import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
@@ -30,8 +32,9 @@ public class ChapterTask implements Runnable {
     public void run() {
         try {
             Document doc = Jsoup.connect(this.chapter.getUrl()).get();
-            String text = Objects.requireNonNull(doc.selectFirst("#htmlContent")).text();
-            this.chapter.setText(Discard.handle(text));
+            Element element = Objects.requireNonNull(doc.selectFirst("#htmlContent"));
+            String text = getText(element);
+            this.chapter.setText(Replace.handle(text));
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -45,5 +48,22 @@ public class ChapterTask implements Runnable {
             System.out.print("\n" + String.format("进度: %s ", (total - count) * 100 / total) + "%");
         }
         System.out.print("#");
+    }
+
+    private String getText(Element parentElement) {
+        StringBuilder working = new StringBuilder();
+        for (Node child : parentElement.childNodes()) {
+            if (child instanceof TextNode) {
+                working.append(((TextNode) child).text());
+            }
+            if (child instanceof Element) {
+                Element childElement = (Element) child;
+                if (childElement.tag().getName().equalsIgnoreCase("br")) {
+                    working.append("\n");
+                }
+                working.append(getText(childElement));
+            }
+        }
+        return working.toString();
     }
 }
